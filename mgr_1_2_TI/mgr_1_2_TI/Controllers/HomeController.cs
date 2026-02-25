2c4f841b2ebf8b553cced9f7afef24aa38f1cbec
Goliaten <ognikat4@gmail.com>
2026-02-25 08:43:05 +0100

    initial commit

---
+++ mgr_1_2_TI/mgr_1_2_TI/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using mgr_1_2_TI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace mgr_1_2_TI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ mgr_1_2_TI/mgr_1_2_TI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mgr_1_2_TI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
