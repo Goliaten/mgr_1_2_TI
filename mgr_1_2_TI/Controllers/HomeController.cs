216eeda6b9776520ff8ec80a1f4aa8e8bc954988
Goliaten <ognikat4@gmail.com>
2026-02-25 09:23:31 +0100

    remove ILogger from HomeController

--- mgr_1_2_TI/Controllers/HomeController.cs
+++ mgr_1_2_TI/Controllers/HomeController.cs
@@ -1,18 +1,11 @@
 using System.Diagnostics;
-using mgr_1_2_TI.Models;
+// using mgr_1_2_TI.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace mgr_1_2_TI.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
