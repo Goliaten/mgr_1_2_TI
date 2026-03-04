6b40d018026681cfa72dbc00a3f6c9bab3578333
Goliaten <ognikat4@gmail.com>
2026-03-04 09:18:00 +0100

    update migration scripts

--- scripts/add migration.sh
+++ scripts/add migration.sh
@@ -1,2 +1,6 @@
-cd ..
-dotnet ef migrations add InitialCreate
+#!bin/bash
+
+migr_name="migr_$(date +'%Y%m%d_%H%M%S')"
+cd ../mgr_1_2_TI
+dotnet ef migrations add
+cd ../scripts
--- scripts/apply migration.sh
+++ scripts/apply migration.sh
@@ -1,2 +1,5 @@
-cd ..
+#!bin/bash
+
+cd ../mgr_1_2_TI
 dotnet ef database update
+cd ../scripts
