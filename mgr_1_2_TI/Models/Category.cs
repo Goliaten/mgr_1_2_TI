555e76114739f1b371ff4516860f52cb77401b0b
Goliaten <ognikat4@gmail.com>
2026-03-04 08:22:12 +0100

    add data annotations for category

--- mgr_1_2_TI/Models/Category.cs
+++ mgr_1_2_TI/Models/Category.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,11 @@ namespace mgr_1_2_TI.Models
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
+        [Required(ErrorMessage ="Missing 'Name' field")]
         public required string Name { get; set; }
+        [StringLength(512)]
         public string? Description { get; set; }
         public ICollection<Movie>? Movies { get; set; }
     }
