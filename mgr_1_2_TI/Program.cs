1ee1db0e958aba6d61c2a7d60f8113b21803d36f
Goliaten <ognikat4@gmail.com>
2026-03-04 08:54:48 +0100

    add dockerised database and alter connection string

--- mgr_1_2_TI/Program.cs
+++ mgr_1_2_TI/Program.cs
@@ -1,7 +1,11 @@
+using mgr_1_2_TI.DAL;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<MovieContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
