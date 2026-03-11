d9c3d3e92482b5ee630b3ebdf5b3d0fe4091c062
Goliaten <ognikat4@gmail.com>
2026-03-11 09:11:55 +0100

    Dynamic navbar

--- mgr_1_2_TI/Controllers/HomeController.cs
+++ mgr_1_2_TI/Controllers/HomeController.cs
@@ -1,14 +1,18 @@
 using System.Diagnostics;
+using mgr_1_2_TI.DAL;
 using mgr_1_2_TI.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace mgr_1_2_TI.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController(MovieContext db) : Controller
     {
+        MovieContext db = db;
+
         public IActionResult Index()
         {
-            return View();
+            var categories = db.T_Categories.ToList();
+            return View(categories);
         }
 
         public IActionResult FooterSites(string viewName)
