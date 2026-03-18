8b87ae1823f505fcf93f1f97d8a98a5c4496fdca
Goliaten <ognikat4@gmail.com>
2026-03-18 08:35:59 +0100

    shared dynamic category navbar

--- mgr_1_2_TI/ViewComponents/MenuViewComponent.cs
+++ mgr_1_2_TI/ViewComponents/MenuViewComponent.cs
@@ -2,11 +2,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using mgr_1_2_TI.DAL;
+using Microsoft.AspNetCore.Mvc;
 
 namespace mgr_1_2_TI.ViewComponents
 {
-    public class MenuViewComponent
+    public class MenuViewComponent : ViewComponent
     {
-        
+        MovieContext db;
+
+        public MenuViewComponent(MovieContext db)
+        {
+            this.db = db;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return await Task.FromResult(View("Menu", db.T_Categories.ToList())); // # FIXME missing view name 
+        }
     }
 }
