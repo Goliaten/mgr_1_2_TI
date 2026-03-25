718a28bf90ce87c3b2ec5106a8404b5a3e401ac0
Goliaten <ognikat4@gmail.com>
2026-03-25 08:58:37 +0100

    formatting

--- mgr_1_2_TI/Models/Category.cs
+++ mgr_1_2_TI/Models/Category.cs
@@ -10,10 +10,10 @@ namespace mgr_1_2_TI.Models
     {
         [Key]
         public int Id { get; set; }
-        [Required(ErrorMessage ="Missing 'Name' field")]
+        [Required(ErrorMessage = "Missing 'Name' field")]
         public required string Name { get; set; }
         [StringLength(512)]
         public string? Description { get; set; }
-        public ICollection<Movie>? Movies { get; set; }
+        public ICollection<Movie> Movies { get; set; }
     }
 }
--- mgr_1_2_TI/Models/Movie.cs
+++ mgr_1_2_TI/Models/Movie.cs
@@ -11,16 +11,16 @@ namespace mgr_1_2_TI.Models
     {
         [Key]
         public int Id { get; set; }
-        [Required(ErrorMessage ="Missing 'Title' field")]
+        [Required(ErrorMessage = "Missing 'Title' field")]
         public required string Title { get; set; }
         [StringLength(512)]
         public string? Description { get; set; }
         public string? Director { get; set; }
-        [Required(ErrorMessage ="Missing 'Price' field")]
+        [Required(ErrorMessage = "Missing 'Price' field")]
         public decimal Price { get; set; }
         // [ForeignKey('Category')]
         [ForeignKey(nameof(Category))]
         public int CategoryId { get; set; }
-        public Category? Category { get; set; }
+        public Category Category { get; set; }
     }
 }
