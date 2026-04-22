1e0657e0c3146339799f114903bf9099cff8cfb5
Goliaten <ognikat4@gmail.com>
2026-04-22 08:53:11 +0200

    enumerator for basket statics

---
+++ mgr_1_2_TI/Enums/BasketEnum.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mgr_1_2_TI.Enums
+{
+    public static class BasketEnum
+    {
+        public static string CartSessionKey = "sessionKey";
+    }
+}
