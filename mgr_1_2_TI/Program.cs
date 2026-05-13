f259d6b93e949b7c3a7c339cf9a9fd230fc28b5f
Goliaten <ognikat4@gmail.com>
2026-05-13 08:37:46 +0200

    add identity services and authentication

--- mgr_1_2_TI/Program.cs
+++ mgr_1_2_TI/Program.cs
@@ -1,4 +1,5 @@
 using mgr_1_2_TI.DAL;
+using mgr_1_2_TI.Models.Users;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +7,18 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<MovieContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<AccountContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddIdentity<WebUser, WebUserRole>(options =>
+{
+    options.User.RequireUniqueEmail = true;
+    //FIXME password was made weak for testing purposes 
+    options.Password.RequiredLength = 2;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredUniqueChars = 0;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireDigit = false;
+}).AddEntityFrameworkStores<AccountContext>();
 builder.Services.AddSession();
 
 var app = builder.Build();
@@ -23,6 +36,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication(); // has to be before authorization
 app.UseAuthorization();
 app.UseSession();
 
